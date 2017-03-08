7ca1d78311dbd1a5a47a887453010d73ce90d77a
flagroth <javier.cano.montero@urjc.es>
2017-03-08 18:35:49 +0100

    Falta ponerla bonita

--- R/grafos_1.R
+++ R/grafos_1.R
@@ -1,29 +1,49 @@
 library(igraph)
 
-
-df <- read.table("data/out.ego-twitter")
+df <- read.table("/home/javier/practica_1_grafos/data/out.ego-twitter")
 g <- graph_from_data_frame(df)
-g <- simplify(g)
+rm(df)
 
-# V(g)$size <- 8
-# V(g)$frame.color <- "white"
-# V(g)$color <- "orange"
-# V(g)$label <- "" 
-# E(g)$arrow.mode <- 0
-# l <- layout_with_kk(g,maxiter = 100)
-# plot(g, edge.arrow.size=.4,vertex.label=NA,layout=l)
+V(g)$size <- 8
+V(g)$frame.color <- "white"
+V(g)$color <- "orange"
+V(g)$label <- "" 
+E(g)$arrow.mode <- 1
+plot(g)
 
-deg <- degree(g)
-summary(deg)
-
-# sg <- subgraph(g, c(190,191,192,193,194,13167))
+hist(degree(g))
+summary(degree(g))
 
 sg <- induced_subgraph(g, V(g)[which(V(g)$name %in% c("190","191","192","193","194","13167"))])
 plot(sg)
+
+# No es fuertemente conexo
 
 # Max followers
 which.max(degree(g, mode="in"))
 # Max following
 which.max(degree(g, mode="out"))
 
-clique_num(g)
+adjacent_vertices(g,V(g)[V(g)$name=="1305"],mode="in")
+
+adjacent_vertices(g,V(g)[V(g)$name=="1373"],mode="out")
+
+id <- V(g)[V(g)$name=="13815"]
+length(adjacent_vertices(g,id,mode="in")[[1]])/length(adjacent_vertices(g,id,mode="out")[[1]])
+
+is.connected(g)
+
+cl <- cluster_infomap(g)
+print(sizes(cl))
+
+id_com <- which(sizes(cl)==35)
+sg <- induced_subgraph(g,V(g)[cl$membership==id_com])
+degree(sg)
+plot(sg)
+
+id_max <- which.max(sizes(cl))
+sg <- induced_subgraph(g,V(g)[cl$membership==id_max])
+which.max(closeness(sg))
+
+which.max(betweenness(sg))
+
